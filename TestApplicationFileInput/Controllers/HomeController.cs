8d6aabb40b4b27b0ac30fc8aa93383ed45316e04
Muhammad Waqas Aziz <thandaprogramer@outlook.com>
2018-09-18 10:25:57 +0500

    Committing Project

---
+++ TestApplicationFileInput/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestApplicationFileInput.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Employee(FormCollection data)
+        {
+            List<string> attachs = data.AllKeys.Where(k => k.Contains("Attach")).ToList();
+
+            foreach (var attach in attachs)
+            {
+                string x = data[attach];
+            }
+
+            return View();
+        }
+    }
+}
